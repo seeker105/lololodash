431bd16410deae0738f4815028e055c7a0cd30e5
Christopher Soden <karavol27@gmail.com>
2016-08-09 10:43:40 -0600

    Getting Started is done

--- program.js
+++ program.js
@@ -1,13 +1,7 @@
 var _ = require("lodash");
 
-var theArray = [
-    { id: 22, username: "martin", active: true},
-    { id: 23, username: "max",    active: false},
-    { id: 24, username: "linda",  active: false}
-];
-
-var worker = function( incoming ){
-  _.filter.characters(incoming, 'active': true)
+var worker = function(incoming){
+  return _.filter(incoming, {active: true})
 };
 
 module.exports = worker;
