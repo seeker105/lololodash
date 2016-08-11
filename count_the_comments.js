1c4f4e3f2fbda45e4a7c011bbe96c9d45bde7b36
Christopher Soden <karavol27@gmail.com>
2016-08-11 10:42:00 -0600

    Count the comments done

--- count_the_comments.js
+++ count_the_comments.js
@@ -1,7 +1,17 @@
 var _ = require("lodash");
 
-function worker (){
-  return 
+function worker (comments){
+  newArray = [];
+  groupedComments = _.groupBy(comments, 'username');
+  _.forEach(groupedComments, function(group, key){
+    count = _.size(group),
+    newArray.push({ username: key, comment_count: count})
+  });
+
+  result = _.sortBy(newArray, "comment_count").reverse()
+  return result
+
+
 }
 
-module.extracts = worker;
+module.exports = worker;
